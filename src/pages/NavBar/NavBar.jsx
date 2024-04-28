d40712837712ac944c3c76b7b8ac4cf464db7a33
digonto0912 <fardulislamdigonto799@gmail.com>
2024-04-29 01:12:11 +0600

    nav respon

--- src/pages/NavBar/NavBar.jsx
+++ src/pages/NavBar/NavBar.jsx
@@ -1,6 +1,14 @@
+import React, { useState } from 'react';
 import "./NavBar.css";
+import menuIcon from "../../../public/images/menu.png";
 
 const NavBar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(prevState => !prevState);
+    };
+
     return (
         <div className="NavBar">
             <div className="navNameSide">Fardul Islam</div>
@@ -11,6 +19,25 @@ const NavBar = () => {
                 <div><a href="/blog">BLOG</a></div>
                 <div><a href="/contact-info">CONTACT INFO</a></div>
                 <div className="resumeBtn"><a href="/resume">MY RESUME</a></div>
+            </div>
+
+            {/* mobile nav */}
+            <div className="mobileNav">
+
+                <div id="showButton" className="mobileNavBtnIcon" onClick={toggleMenu}>
+                    <img src={menuIcon} className="h60" alt="Menu" />
+                </div>
+
+                {showMenu && (
+                    <div className="mobileNavOptions">
+                        <a href="/"><div>HOME</div></a>
+                        <a href="/about"><div>ABOUT</div></a>
+                        <a href="/projects"><div>PROJECTS</div></a>
+                        <a href="/blog"><div>BLOG</div></a>
+                        <a href="/contact-info"><div>CONTACT INFO</div></a>
+                        <a href="/resume" className="resumeBtn"><div>MY RESUME</div></a>
+                    </div>
+                )}
             </div>
         </div>
     );
