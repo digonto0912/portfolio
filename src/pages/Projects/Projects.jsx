f9419260c531dc3928db9b5b693ba4b993bcb114
digonto0912 <fardulislamdigonto799@gmail.com>
2024-06-21 20:10:23 +0600

    fff

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -18,25 +18,31 @@ const Projects = () => {
                 </div>
                 <div className="ProjectsList">
                     <div className="Project">
-                        <img src="https://emeritus.org/in/wp-content/uploads/sites/3/2022/11/02-CaseStudy_PMBlog_shutterstock_422213704-1024x632.jpg.optimal.jpg" alt="" />
+                        <img src="https://d502jbuhuh9wk.cloudfront.net/courses/660db627f5fd821663d46a01/660db627f5fd821663d46a01_scaled_cover.jpg?v=1" alt="" />
                         <div>12 12 april</div>
                         <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
                         <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
                     </div>
                     <div className="Project">
-                        <img src="https://emeritus.org/in/wp-content/uploads/sites/3/2022/11/02-CaseStudy_PMBlog_shutterstock_422213704-1024x632.jpg.optimal.jpg" alt="" />
+                        <img src="https://d502jbuhuh9wk.cloudfront.net/courses/660da6b1cc8b3658a82cf506/660da6b1cc8b3658a82cf506_scaled_cover.jpg?v=1" alt="" />
                         <div>12 12 april</div>
                         <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
                         <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
                     </div>
                     <div className="Project">
-                        <img src="https://emeritus.org/in/wp-content/uploads/sites/3/2022/11/02-CaseStudy_PMBlog_shutterstock_422213704-1024x632.jpg.optimal.jpg" alt="" />
+                        <img src="https://d502jbuhuh9wk.cloudfront.net/courses/660dad017a92fb75afc0fe04/660dad017a92fb75afc0fe04_scaled_cover.jpg?v=1" alt="" />
                         <div>12 12 april</div>
                         <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
                         <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
                     </div>
                     <div className="Project">
-                        <img src="https://emeritus.org/in/wp-content/uploads/sites/3/2022/11/02-CaseStudy_PMBlog_shutterstock_422213704-1024x632.jpg.optimal.jpg" alt="" />
+                        <img src="https://d502jbuhuh9wk.cloudfront.net/courses/660a23f6678c54545891f97e/660a23f6678c54545891f97e_scaled_cover.jpg?v=1" alt="" />
+                        <div>12 12 april</div>
+                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+                        <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
+                    </div>
+                    <div className="Project">
+                        <img src="https://d502jbuhuh9wk.cloudfront.net/courses/660a9a1d7908621df055074f/660a9a1d7908621df055074f_scaled_cover.jpg?v=1" alt="" />
                         <div>12 12 april</div>
                         <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
                         <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
