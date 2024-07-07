ef92eb0147e18bd701d9f3ce78fc6738e52645d4
digonto0912 <fardulislamdigonto799@gmail.com>
2024-07-07 18:30:56 +0600

    abcd1234

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,11 +1,20 @@
 import "./Projects.css";
-import img1 from "../../../public/images/nlp-nltk.png";
-import img2 from "../../../public/images/TSA.png";
-import img3 from "../../../public/images/CS1.png";
+import img1 from "../../../public/images/gradient descent batch.png";
+import img2 from "../../../public/images/insta-bot-2.png";
+import img3 from "../../../public/images/insta-bot-1.png";
 import img4 from "../../../public/images/CS2.png";
-import img5 from "../../../public/images/insta-bot-1.png";
-import img6 from "../../../public/images/insta-bot-2.png";
-import img7 from "../../../public/images/gradient descent batch.png";
+import img5 from "../../../public/images/CS1.png";
+import img6 from "../../../public/images/TSA.png";
+import img7 from "../../../public/images/nlp-nltk.png";
+import img8 from "../../../public/images/img1.jpg";
+import img9 from "../../../public/images/img2.jpg";
+import img10 from "../../../public/images/img3.jpg";
+import img11 from "../../../public/images/img4.jpg";
+import img12 from "../../../public/images/img5.jpg";
+import img13 from "../../../public/images/img6.jpg";
+import img14 from "../../../public/images/img7.jpg";
+import img15 from "../../../public/images/img8.jpg";
+import img16 from "../../../public/images/img9.jpg";
 
 const Projects = () => {
     return (
@@ -25,30 +34,120 @@ const Projects = () => {
                 </div>
                 <div className="ProjectsList">
 
-                    {/* 1 */}
+                    {/* 16 */}
+                    <a href="#" target="_blank">
+                        <div className="Project">
+                            <img src={img16} alt="" />
+                            <div>12 12 april</div>
+                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
+                        </div>
+                    </a>
+
+                    {/* 15 */}
+                    <a href="#" target="_blank">
+                        <div className="Project">
+                            <img src={img15} alt="" />
+                            <div>12 12 april</div>
+                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
+                        </div>
+                    </a>
+
+                    {/* 14 */}
+                    <a href="#" target="_blank">
+                        <div className="Project">
+                            <img src={img14} alt="" />
+                            <div>12 12 april</div>
+                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
+                        </div>
+                    </a>
+                    
+                    {/* 13 */}
+                    <a href="#" target="_blank">
+                        <div className="Project">
+                            <img src={img13} alt="" />
+                            <div>12 12 april</div>
+                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
+                        </div>
+                    </a>
+
+                    {/* 12 */}
+                    <a href="#" target="_blank">
+                        <div className="Project">
+                            <img src={img12} alt="" />
+                            <div>12 12 april</div>
+                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
+                        </div>
+                    </a>
+                    
+                    {/* 11 */}
+                    <a href="#" target="_blank">
+                        <div className="Project">
+                            <img src={img11} alt="" />
+                            <div>12 12 april</div>
+                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
+                        </div>
+                    </a>
+
+                    {/* 10 */}
+                    <a href="#" target="_blank">
+                        <div className="Project">
+                            <img src={img10} alt="" />
+                            <div>12 12 april</div>
+                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
+                        </div>
+                    </a>
+
+                    {/* 9 */}
+                    <a href="#" target="_blank">
+                        <div className="Project">
+                            <img src={img9} alt="" />
+                            <div>12 12 april</div>
+                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
+                        </div>
+                    </a>
+
+                    {/* 8 */}
+                    <a href="#" target="_blank">
+                        <div className="Project">
+                            <img src={img8} alt="" />
+                            <div>12 12 april</div>
+                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
+                        </div>
+                    </a>
+
+                    {/* 7 */}
                     <a href="https://github.com/digonto0912/NLP-with-NLTK" target="_blank">
                         <div className="Project">
-                            <img src={img1} alt="" />
+                            <img src={img7} alt="" />
                             <div>12 12 april</div>
                             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
                             <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
                         </div>
                     </a>
 
-                    {/* 2 */}
+                    {/* 6 */}
                     <a href="https://github.com/digonto0912/twitter-sentiment-analysis" target="_blank">
                         <div className="Project">
-                            <img src={img2} alt="" />
+                            <img src={img6} alt="" />
                             <div>12 12 april</div>
                             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
                             <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
                         </div>
                     </a>
 
-                    {/* 3 */}
+                    {/* 5 */}
                     <a href="https://github.com/digonto0912/case-study-1" target="_blank"> 
                         <div className="Project">
-                            <img src={img3} alt="" />
+                            <img src={img5} alt="" />
                             <div>12 12 april</div>
                             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
                             <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
@@ -65,30 +164,30 @@ const Projects = () => {
                         </div>
                     </a>
 
-                    {/* 5 */}
+                    {/* 3 */}
                     <a href="https://github.com/digonto0912/Instagram-Bot---1" target="_blank">
                         <div className="Project">
-                            <img src={img5} alt="" />
+                            <img src={img3} alt="" />
                             <div>12 12 april</div>
                             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
                             <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
                         </div>
                     </a>
 
-                    {/* 6 */}
+                    {/* 2 */}
                     <a href="https://github.com/digonto0912/Instagram-Bot---2" target="_blank">
                         <div className="Project">
-                            <img src={img6} alt="" />
+                            <img src={img2} alt="" />
                             <div>12 12 april</div>
                             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
                             <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
                         </div>
                     </a>
 
-                    {/* 7 */}
+                    {/* 1 */}
                     <a href="https://github.com/digonto0912/gradient-descent-batch-from-scratch" target="_blank">
                         <div className="Project">
-                            <img src={img7} alt="" />
+                            <img src={img1} alt="" />
                             <div>12 12 april</div>
                             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
                             <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptate natus eos? Eaque sunt possimus numquam quia obcaecati delectus non molestias dicta asperiores enim libero, distinctio totam alias provident repudiandae.</h5>
